00085dad904477f64de65cf28c47579b3e6921c6
liuziangexit <liuziang@liuziangexit.com>
2020-12-18 21:07:48 +0800

    inital commit.

---
+++ src/main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>            // for standard I/O
+#include <opencv2/core.hpp>    // Basic OpenCV structures (cv::Mat)
+#include <opencv2/videoio.hpp> // Video write
+#include <stdio.h>
+#include <string> // for strings
+
+void main(int argc, char **argv) { return 0; }
---
+++ tools/capture.sh
@@ -0,0 +1,1 @@
+fswebcam -r 1280x720 --jpeg -100 test.jpg
