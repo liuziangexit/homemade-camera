5e336e722d77c97c3ab09369b0799c9349fded63
liuziangexit <liuziang@liuziangexit.com>
2021-01-04 16:27:25 +0800

    no message

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,6 @@
-//#include "capture.h"
-#include "ffmpeg_capture.h"
-#include "ffmpeg_encoder.h"
+#include "capture.h"
 #include "logger.h"
-#include <fstream>
-#include <opencv2/core.hpp>
-#include <opencv2/highgui.hpp>
+#include <signal.h>
 #include <web_service.h>
 
 homemadecam::web *web;
@@ -18,27 +14,6 @@ void signal_handler(int signum) {
 }
 
 int main(int argc, char **argv) {
-
-  homemadecam::ffmpeg_capture cap;
-  int ret = cap.open("avfoundation", "0", cv::Size{1280, 720}, 30);
-  // cv::imshow("Display window", frame);
-
-  std::ofstream file("test.mov", std::ios_base::out);
-  homemadecam::ffmpeg_encoder<false> enc;
-
-  ret = enc.open(homemadecam::H264, cap.codec_context, file);
-
-  for (int i = 0; i < 100; i++) {
-    auto frame = cap.read();
-    enc.write(frame);
-  }
-
-  cap.close();
-  enc.close();
-
-  file.close();
-  // cv::waitKey(0); // Wait for a keystroke in the window
-  /*
   signal(SIGINT, signal_handler);
   cv::setNumThreads(0);
   web = new homemadecam::web("config.json");
@@ -49,5 +24,5 @@ int main(int argc, char **argv) {
   cap->run();
 
   getchar();
-  raise(SIGINT);*/
+  raise(SIGINT);
 }
