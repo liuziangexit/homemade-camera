d85875f72a793f6b69f738d76782c8ff62fc34b5
liuziangexit <liuziang@liuziangexit.com>
2021-01-06 20:35:56 +0800

    build.sh

--- 3rd-party/ilclient/build.sh
+++ 3rd-party/ilclient/build.sh
@@ -1,2 +1,4 @@
-cp -ru /opt/vc/src/hello_pi/libs/ilclient/* .
-make
+mkdir -p libs
+cp -ru /opt/vc/src/hello_pi/libs/ilclient libs
+cp -u /opt/vc/src/hello_pi/Makefile.include .
+make -C libs/ilclient
