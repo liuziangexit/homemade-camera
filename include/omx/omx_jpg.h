6dd1018bd463fbbcf0cd05e6279ba667d55fbc73
liuziangexit <liuziang@liuziangexit.com>
2021-01-07 00:48:53 +0800

    no message

--- include/omx/omx_jpg.h
+++ include/omx/omx_jpg.h
@@ -12,6 +12,7 @@ extern "C" {
 
 #include <assert.h>
 #include <cstddef>
+#include <opencv2/core/core_c.h>
 #include <opencv2/core/mat.hpp>
 #include <opencv2/imgcodecs.hpp>
 #include <opencv2/imgproc.hpp>
