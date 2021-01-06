1f422503137cf7e9fee43312d0ac7fd93ed10dcb
liuziangexit <liuziang@liuziangexit.com>
2021-01-06 20:48:56 +0800

    no message

--- build.sh
+++ build.sh
@@ -1,4 +1,7 @@
 mkdir -p bin
 cmake -DCMAKE_BUILD_TYPE=DEBUG -S . -B bin
+cd 3rd-party/ilclient
+source build.sh
+cd ../..
 cd bin
 make -j4 VERBOSE=1
--- clean.sh
+++ clean.sh
@@ -1,1 +1,4 @@
 rm -rf bin
+cd 3rd-party/ilclient
+source clean.sh
+cd ../..
