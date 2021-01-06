2c0ee136c7d0bb70f62934683e5f456bccb6fcac
liuziangexit <liuziang@liuziangexit.com>
2021-01-06 23:33:10 +0800

    www

--- build.sh
+++ build.sh
@@ -4,9 +4,11 @@ if [[ ! $# -eq 0 ]]; then
   BUILD_TYPE="$1"
 fi
 
-cmake -DCMAKE_BUILD_TYPE="$BUILD_TYPE" -S . -B bin
 cd 3rd-party/ilclient
 source build.sh
 cd ../..
+
+cmake -DCMAKE_BUILD_TYPE="$BUILD_TYPE" -S . -B bin
+
 cd bin
 make -j4 VERBOSE=1
