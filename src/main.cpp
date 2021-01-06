8427cde9c92ffe7cf13fd8f3ed8ae7251b56548c
liuziangexit <liuziang@liuziangexit.com>
2021-01-06 16:18:40 +0800

    test

--- src/main.cpp
+++ src/main.cpp
@@ -1,63 +1,44 @@
-#include "capture.h"
-#include "logger.h"
-#include "v4l_capture.h"
-#include <signal.h>
 #include <stdio.h>
-//#include <web_service.h>
-
-// homemadecam::web *web;
-homemadecam::capture *cap;
-
-void signal_handler(int signum) {
-  homemadecam::logger::info("signal ", signum, " received, quitting...");
-  //  delete web;
-  delete cap;
-  exit(signum);
+#include <string.h>
+#include <fcntl.h>
+#include "linux/videodev2.h"
+void printFrameInterval(int fd, unsigned int fmt, unsigned int width, unsigned int height)
+{
+  struct v4l2_frmivalenum frmival;
+  memset(&frmival,0,sizeof(frmival));
+  frmival.pixel_format = fmt;
+  frmival.width = width;
+  frmival.height = height;
+  while (ioctl(fd, VIDIOC_ENUM_FRAMEINTERVALS, &frmival) == 0)
+  {
+    if (frmival.type == V4L2_FRMIVAL_TYPE_DISCRETE)
+      printf("[%dx%d] %f fps\n", width, height, 1.0*frmival.discrete.denominator/frmival.discrete.numerator);
+    else
+      printf("[%dx%d] [%f,%f] fps\n", width, height, 1.0*frmival.stepwise.max.denominator/frmival.stepwise.max.numerator, 1.0*frmival.stepwise.min.denominator/frmival.stepwise.min.numerator);
+    frmival.index++;
+  }
 }
 
 int main(int argc, char **argv) {
-  // cv::utils::logging::setLogLevel(cv::utils::logging::LOG_LEVEL_VERBOSE);
-
-  signal(SIGINT, signal_handler);
-  //  web = new homemadecam::web("config.json");
-  //  web->run();
-
-  /*  homemadecam::config c("config.json");
-    cap = new homemadecam::capture(c);
-    cap->run();*/
-
-  homemadecam::v4l_capture v4l;
-  if (v4l.open("/dev/video0", 30)) {
-    homemadecam::logger::fatal("v4l open failed");
-    abort();
-  }
-
-  std::shared_ptr<homemadecam::v4l_capture::buffer> jpg;
-  for (int i = 0; i < 300; i++) {
-    auto frame = v4l.read();
-    if (!frame.first) {
-      homemadecam::logger::fatal("v4l read failed");
-      abort();
+  unsigned int width=0, height=0;;
+  int fd = open("/dev/video0", O_RDWR);
+  struct v4l2_frmsizeenum frmsize;
+  memset(&frmsize,0,sizeof(frmsize));
+  frmsize.pixel_format = V4L2_PIX_FMT_JPEG;
+  while (ioctl(fd, VIDIOC_ENUM_FRAMESIZES, &frmsize) == 0)
+  {
+    if (frmsize.type == V4L2_FRMSIZE_TYPE_DISCRETE)
+    {
+      printFrameInterval(fd, frmsize.pixel_format, frmsize.discrete.width, frmsize.discrete.height);
     }
-    jpg = frame.second;
+    else
+    {
+      for (width=frmsize.stepwise.min_width; width< frmsize.stepwise.max_width; width+=frmsize.stepwise.step_width)
+        for (height=frmsize.stepwise.min_height; height< frmsize.stepwise.max_height; height+=frmsize.stepwise.step_height)
+          printFrameInterval(fd, frmsize.pixel_format, width, height);
+
+    }
+    frmsize.index++;
   }
-
-  FILE *fp;
-  fp = fopen("/web/test.jpg", "wb");
-  if (!fp) {
-    homemadecam::logger::fatal("fopen failed");
-    abort();
-  }
-
-  if (jpg->length != fwrite(jpg->data, 1, jpg->length, fp)) {
-    homemadecam::logger::fatal("fwrite failed");
-    abort();
-  }
-
-  fclose(fp);
-
-  homemadecam::logger::info("jpg ok");
-
-  getchar();
-  raise(SIGINT);
+  return 0;
 }
