21eb9387c0a8269c1e268706e99e99ec17ba0122
liuziangexit <liuziang@liuziangexit.com>
2021-01-07 20:16:00 +0800

    回复一下正式的main

--- src/main.cpp
+++ src/main.cpp
@@ -1,66 +1,26 @@
-#include "omx/omx_jpg.h"
-#include "omx/omx_lib.h"
 #include "util/logger.h"
 #include "video/capture.h"
-#include "video/v4l_capture.h"
-#include <opencv2/imgcodecs.hpp>
+//#include "web/service.h"
 #include <signal.h>
-#include <stdio.h>
-//#include <web_service.h>
 
 // homemadecam::web *web;
 homemadecam::capture *cap;
 
 void signal_handler(int signum) {
   homemadecam::logger::info("signal ", signum, " received, quitting...");
-  //  delete web;
+  // delete web;
   delete cap;
   exit(signum);
 }
 
 int main(int argc, char **argv) {
-  // cv::utils::logging::setLogLevel(cv::utils::logging::LOG_LEVEL_VERBOSE);
-
   signal(SIGINT, signal_handler);
-  //  web = new homemadecam::web("config.json");
-  //  web->run();
+  /*web = new homemadecam::web("config.json");
+  web->run();*/
 
-  /*  homemadecam::config c("config.json");
-    cap = new homemadecam::capture(c);
-    cap->run();*/
-
-  homemadecam::v4l_capture v4l;
-  if (v4l.open("/dev/video0",
-               homemadecam::v4l_capture::graphic{1280, 720, 30})) {
-    homemadecam::logger::fatal("v4l open failed");
-    abort();
-  }
-
-  std::shared_ptr<homemadecam::v4l_capture::buffer> jpg;
-  homemadecam::logger::info("read loop begin");
-  for (int i = 0; i < 2; i++) {
-    auto frame = v4l.read();
-    if (!frame.first) {
-      homemadecam::logger::fatal("v4l read failed");
-      abort();
-    }
-    jpg = frame.second;
-  }
-  homemadecam::logger::info("read loop end");
-
-  homemadecam::omx_lib lib;
-  homemadecam::omx_jpg omx;
-  auto decode =
-      omx.decode(static_cast<unsigned char *>(jpg->data), jpg->length);
-  if (!decode.first) {
-    homemadecam::logger::fatal("decode failed");
-    abort();
-  }
-
-  cv::imwrite("test.jpg", decode.second);
-  cv::imwrite("test.png", decode.second);
-
-  homemadecam::logger::info("jpg ok");
+  homemadecam::config c("config.json");
+  cap = new homemadecam::capture(c);
+  cap->run();
 
   getchar();
   raise(SIGINT);
