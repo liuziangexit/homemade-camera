cf1e71f6263929f0e548dccb6c2314a2def723a7
liuziangexit <liuziang@liuziangexit.com>
2021-01-19 23:08:56 +0800

    去掉mtrace

--- src/main.cpp
+++ src/main.cpp
@@ -2,8 +2,6 @@
 #include "video/capture.h"
 //#include "web/service.h"
 #include "omx/omx_lib.h"
-#include <iostream>
-#include <mcheck.h>
 #include <signal.h>
 
 // hcam::web *web;
@@ -12,26 +10,21 @@ hcam::capture *cap;
 void signal_handler(int signum) {
   hcam::logger::info("signal ", signum, " received, quitting...");
   // delete web;
-  // delete cap;
-
+  delete cap;
   exit(signum);
 }
-
-hcam::omx_lib omxlib;
-hcam::config c("config.json");
 
 int main(int argc, char **argv) {
   signal(SIGINT, signal_handler);
   /*web = new hcam::web("config.json");
   web->run();*/
 
-  mtrace();
+  hcam::omx_lib omxlib;
 
+  hcam::config c("config.json");
   cap = new hcam::capture(c);
   cap->run();
 
   getchar();
-  delete cap;
-  muntrace();
   raise(SIGINT);
 }
