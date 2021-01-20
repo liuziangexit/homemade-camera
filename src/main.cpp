41a577c060e01f75415e93ecf2937c3f1b84ee4c
liuziangexit <liuziang@liuziangexit.com>
2021-01-20 15:18:24 +0800

    main不要提交

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,33 @@
-#include "util/thread_pool.h"
-#include <iostream>
+#include "util/logger.h"
+#include "video/capture.h"
+//#include "web/service.h"
+#include "omx/omx_lib.h"
+#include <signal.h>
+#include <thread>
 
-int main() {
-  hcam::thread_pool tp;
-  tp.run(4);
-  auto fut = tp.async([] {
-    std::cout << "naive" << std::endl;
-    return 999;
-  });
-  fut.wait();
-  // getchar();
-  tp.stop();
-  std::cout << "Hello, World!" << std::endl;
-  return 0;
+// hcam::web *web;
+hcam::capture *cap;
+
+void signal_handler(int signum) {
+  hcam::logger::info("signal ", signum, " received, quitting...");
+  // delete web;
+  delete cap;
+  exit(signum);
+}
+
+int main(int argc, char **argv) {
+  signal(SIGINT, signal_handler);
+  signal(SIGTERM, signal_handler);
+  /*web = new hcam::web("config.json");
+  web->run();*/
+
+  hcam::omx_lib omxlib;
+
+  hcam::config c("config.json");
+  cap = new hcam::capture(c);
+  cap->run();
+
+  std::this_thread::sleep_for(std::chrono::hours(8));
+  /* getchar();*/
+  raise(SIGINT);
 }
