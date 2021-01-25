56c2f3d7461e067522bef5509be326f0f81caae6
liuziangexit <liuziang@liuziangexit.com>
2021-01-25 20:55:55 +0800

    no message

--- include/config/config.h
+++ include/config/config.h
@@ -7,15 +7,13 @@ namespace hcam {
 
 class config {
 public:
-  //这俩暂时没用到，现在直接改opencv里的默认值
-  cv::Size resolution;
-  int fps;
-
   enum codec cam_pix_fmt;    //摄像头的像素格式
   uint32_t duration;         //每个视频文件的持续时间
   std::string save_location; //保存目录
   enum codec output_codec;   //视频文件编码格式
   std::string device;        //摄像头设备挂载点
+  cv::Size resolution;       //分辨率
+  int fps;                   //帧率
   int timestamp_pos; //视频时间戳位置 0-右上 1-左上 2-左下 3-右下 4-中间
   int display_fps; //帧率显示模式 0-NEVER 2-WARNING ONLY 3-ALWAYS
   int font_height; //视频时间戳字体大小
