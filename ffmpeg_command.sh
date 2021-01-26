cc4ff33d49d21dba1983869b08555c28407966b2
liuziangexit <liuziang@liuziangexit.com>
2021-01-26 17:32:38 +0800

    no message

--- ffmpeg_command.sh
+++ ffmpeg_command.sh
@@ -1,1 +1,3 @@
-ffmpeg -i ./demo.mov -c:v h264_omx -vf format=yuv420p -b:v 2048k demo_out_ff.mov
+ffmpeg -f v4l2 -input_format mjpeg \
+ -framerate 30 -video_size 1280x720 -i /dev/video0 \
+ -c:v h264_omx -vf format=yuv420p -b:v 2048k output.mov
