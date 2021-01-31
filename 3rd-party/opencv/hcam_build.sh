e1f1be9fd4ac547d8e96222b3d665e17d056e896
liuziangexit <liuziang@liuziangexit.com>
2021-01-31 23:39:20 +0800

    no message

--- 3rd-party/opencv/hcam_build.sh
+++ 3rd-party/opencv/hcam_build.sh
@@ -10,7 +10,7 @@ cmake --enable-nonfree --enable-pic --enable-shared \
   -DBUILD_opencv_flann=OFF -DBUILD_opencv_gapi=OFF -DBUILD_opencv_ml=OFF -DBUILD_opencv_objdetect=OFF -DBUILD_opencv_stitching=OFF -DBUILD_opencv_ts=OFF \
   -DBUILD_OPENCV_PYTHON3=OFF -DBUILD_OPENCV_PYTHON2=OFF \
   -DOPENCV_EXTRA_MODULES_PATH=../opencv_contrib/modules/ \
-  -D CMAKE_BUILD_TYPE=RELEASE -D ENABLE_NEON=ON -D ENABLE_VFPV3=ON \
+  -D CMAKE_BUILD_TYPE=RELEASE -DCMAKE_INSTALL_PREFIX:PATH="$(pwd)/install" -D ENABLE_NEON=ON -D ENABLE_VFPV3=ON \
   -S. -Bbuild
 cd build
 make -j4
