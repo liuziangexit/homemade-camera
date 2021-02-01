4dc4bf90ab1ff367b7b7805d41f6f823b27819fc
liuziangexit <liuziang@liuziangexit.com>
2021-02-02 01:08:39 +0800

    软解jpg失败时加点打印

--- src/soft_jpg.cpp
+++ src/soft_jpg.cpp
@@ -1,6 +1,6 @@
 #include "video/soft_jpg.h"
+#include "util/logger.h"
 #include <memory>
-#define CHANNEL_CNT 3
 
 namespace hcam {
 
@@ -8,13 +8,19 @@ soft_jpg::soft_jpg() { _jpegDecompressor = tjInitDecompress(); }
 soft_jpg::~soft_jpg() { tjDestroy(_jpegDecompressor); }
 std::pair<bool, cv::Mat> soft_jpg::decode(unsigned char *src, uint32_t len) {
   int jpegSubsamp, width, height;
-  if (tjDecompressHeader2(_jpegDecompressor, src, len, &width, &height,
-                          &jpegSubsamp))
+  int ret = tjDecompressHeader2(_jpegDecompressor, src, len, &width, &height,
+                                &jpegSubsamp);
+  if (ret) {
+    logger::fatal("tjDecompressHeader2 failed with code ", ret);
     return std::pair<bool, cv::Mat>(false, cv::Mat());
+  }
   cv::Mat mat(height, width, CV_8UC3);
-  if (tjDecompress2(_jpegDecompressor, src, len, mat.data, width, 0 /*pitch*/,
-                    height, TJPF_BGR, TJFLAG_FASTDCT))
+  ret = tjDecompress2(_jpegDecompressor, src, len, mat.data, width, 0 /*pitch*/,
+                      height, TJPF_BGR, TJFLAG_FASTDCT);
+  if (ret) {
+    logger::fatal("tjDecompress2 failed with code ", ret);
     return std::pair<bool, cv::Mat>(false, cv::Mat());
+  }
   return std::pair<bool, cv::Mat>(true, mat);
 }
 
