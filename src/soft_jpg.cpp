2da0d8c2b914cc9059fcb6979fc3a04322d8d4e7
liuziangexit <liuziang@liuziangexit.com>
2021-02-02 10:13:30 +0800

    no message

--- src/soft_jpg.cpp
+++ src/soft_jpg.cpp
@@ -8,17 +8,15 @@ soft_jpg::soft_jpg() { _jpegDecompressor = tjInitDecompress(); }
 soft_jpg::~soft_jpg() { tjDestroy(_jpegDecompressor); }
 std::pair<bool, cv::Mat> soft_jpg::decode(unsigned char *src, uint32_t len) {
   int jpegSubsamp, width, height;
-  int ret = tjDecompressHeader2(_jpegDecompressor, src, len, &width, &height,
-                                &jpegSubsamp);
-  if (ret) {
-    logger::fatal("tjDecompressHeader2 failed with code ", ret);
+  if (tjDecompressHeader2(_jpegDecompressor, src, len, &width, &height,
+                          &jpegSubsamp)) {
+    logger::fatal("tjDecompressHeader2 failed: ", tjGetErrorStr());
     return std::pair<bool, cv::Mat>(false, cv::Mat());
   }
   cv::Mat mat(height, width, CV_8UC3);
-  ret = tjDecompress2(_jpegDecompressor, src, len, mat.data, width, 0 /*pitch*/,
-                      height, TJPF_BGR, TJFLAG_FASTDCT);
-  if (ret) {
-    logger::fatal("tjDecompress2 failed with code ", ret);
+  if (tjDecompress2(_jpegDecompressor, src, len, mat.data, width, 0 /*pitch*/,
+                    height, TJPF_BGR, TJFLAG_FASTDCT)) {
+    logger::fatal("tjDecompress2 failed: ", tjGetErrorStr());
     return std::pair<bool, cv::Mat>(false, cv::Mat());
   }
   return std::pair<bool, cv::Mat>(true, mat);
