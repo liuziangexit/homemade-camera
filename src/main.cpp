21f3d128b435df922bce237409c85c2a18060a99
liuziangexit <liuziang@liuziangexit.com>
2021-02-03 16:26:59 +0800

    修复了main中等待停止信号的问题

--- src/main.cpp
+++ src/main.cpp
@@ -1,36 +1,18 @@
 #include "util/logger.h"
 #include "video/capture.h"
 #include "web/web_service.h"
-#include <atomic>
-#include <condition_variable>
-#include <mutex>
 #include <opencv2/core/utility.hpp>
 #include <signal.h>
-#include <thread>
 
 hcam::web_service *web;
 hcam::capture *cap;
 
-std::atomic<bool> quit = false;
-std::mutex quit_mut;
-std::condition_variable quit_cv;
-
-bool is_quit() { return quit; }
-
 void signal_handler(int signum) {
-  {
-    bool expect = false;
-    if (!quit.compare_exchange_strong(expect, true))
-      return;
-  }
   hcam::logger::info("main", "signal ", signum, " received, quitting...");
   cap->stop();
   web->stop();
   delete cap;
   delete web;
-  std::unique_lock l(quit_mut);
-  quit_cv.notify_one();
-  l.unlock();
   exit(signum);
 }
 
@@ -49,6 +31,5 @@ int main(int argc, char **argv) {
   raise(SIGINT);*/
   // FIXME 卧槽，这就是UB吗？
   // std::this_thread::sleep_for(std::chrono::hours::max());
-  std::unique_lock l(quit_mut);
-  quit_cv.wait(l, is_quit);
+  pause();
 }
