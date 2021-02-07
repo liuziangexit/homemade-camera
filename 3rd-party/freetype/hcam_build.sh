2aaee79871282e3a4ff1c4f8fccfcc0243bb6581
liuziangexit <liuziang@liuziangexit.com>
2021-02-07 23:00:35 +0800

    no message

--- 3rd-party/freetype/hcam_build.sh
+++ 3rd-party/freetype/hcam_build.sh
@@ -1,4 +1,4 @@
-export PKG_CONFIG_PATH="$PKG_CONFIG_PATH:../harfbuzz/install/lib/pkgconfig"
-cmake -DFT_WITH_HARFBUZZ=true -DCMAKE_BUILD_TYPE=RELEASE -DCMAKE_INSTALL_PREFIX:PATH="$(pwd)/install" -S. -Bbuild
+#需要手动改下freetype2.pc
+cmake -DCMAKE_BUILD_TYPE=RELEASE -DCMAKE_INSTALL_PREFIX:PATH="$(pwd)/install" -S. -Bbuild
 cd build
 make install -j4
